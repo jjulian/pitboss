fb9e0669e7c9448a1eaff4087a4508b66ee609d6
Ed Schmalzle <Jonas714@gmail.com>
2010-07-05 13:22:20 -0400

    implement the best hand method

--- lib/pitboss/score.rb
+++ lib/pitboss/score.rb
@@ -1,13 +1,39 @@
 require 'rubygems'
+require 'ruby-poker'
 
 module Pitboss
-  
+
   class Score
 
     def self.best_hand(hand, community_cards)
-      
+      arr = hand + community_cards
+      permutations = []
+      arr.perm(5){ |p| permutations << p}
+      permutations.map!{|p| PokerHand.new(p)}
+      permutations.sort! { |a,b| b <=> a}
+      permutations.first
+    end    
+
+  end
+
+end
+
+class Array
+
+  def perm(n = size)
+    if size < n or n < 0
+    elsif n == 0
+      yield([])
+    else
+      self[1..-1].perm(n - 1) do |x|
+	(0...n).each do |i|
+	  yield(x[0...i] + [first] + x[i..-1])
+	end
+      end
+      self[1..-1].perm(n) do |x|
+	yield(x)
+      end
     end
-    
   end
 
 end
--- spec/score_spec.rb
+++ spec/score_spec.rb
@@ -3,17 +3,13 @@ require 'pitboss'
 
 describe Pitboss::Score do
   
-  it 'should obey the rules of making a hand from a persons hand and community cards' do
-    
-  end
-  
   it 'should make the best possible hand given a players hand and the community cards' do
     hand = %w{AS 3D}
     community_cards =%w{AD 3S 4C 7S 9D}
     
     best_hand = Pitboss::Score.best_hand(hand, community_cards)
     
-    best_hand.should == %w{AS AD 3D 3S 9D}
+    best_hand.should == PokerHand.new(%w{AS AD 3D 3S 9D})
   end
   
 end
