8386338cc0ded6f6628d81de9c768d2667fac5c4
Ed Schmalzle <Jonas714@gmail.com>
2010-07-14 14:00:28 -0400

    scoring should be faster and more efficient credit: @gjastrab

--- lib/pitboss/score.rb
+++ lib/pitboss/score.rb
@@ -5,39 +5,20 @@ module Pitboss
 
   class Score
 
-    def self.best_hand(hands, community_cards)
+    def self.best_hand(hand, community_cards)
       best_combinations = []
-      hands.each do | hand |
-        arr = hand + community_cards
-        permutations = []
-        arr.perm(5){ |p| permutations << p}
-        permutations.map!{|p| PokerHand.new(p)}
-        permutations.sort! { |a,b| b <=> a}
-        best_combinations << permutations.first
+      arr = hand + community_cards
+
+      permutations = [community_cards]
+      permutations.concat(community_cards.combination(3).map{|h| h + hand})
+      hand.each do |card|
+        permutations.concat(community_cards.combination(4).map{|h| h << card})
       end
-      best_combinations.sort{ |a,b| b<=>a}.first
+
+      permutations.map!{|p| PokerHand.new(p)}
+      permutations.sort{ |a,b| b <=> a}.first
     end    
 
-  end
-
-end
-
-class Array
-
-  def perm(n = size)
-    if size < n or n < 0
-    elsif n == 0
-      yield([])
-    else
-      self[1..-1].perm(n - 1) do |x|
-	(0...n).each do |i|
-	  yield(x[0...i] + [first] + x[i..-1])
-	end
-      end
-      self[1..-1].perm(n) do |x|
-	yield(x)
-      end
-    end
   end
 
 end
