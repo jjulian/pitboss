3356b5d48fa844b311e5c32cda900581c0e6dc42
Jonathan Julian <jonathan.m.julian@gmail.com>
2010-07-19 10:44:31 -0400

    implement the Knuth Shuffle algorithm

--- lib/pitboss/deck.rb
+++ lib/pitboss/deck.rb
@@ -24,7 +24,23 @@ module Pitboss
     def initialize
       @cards = %w(C D H S).map do |suit|
         ((2..9).to_a + %w(T J Q K A)).map {|card| Card.new("#{card}#{suit}") }
-      end.flatten.uniq.shuffle
+      end.flatten.uniq.knuth_shuffle!
     end
+  end
+end
+
+# http://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+# ruby sourced from http://rosettacode.org/wiki/Knuth_shuffle#Ruby
+class Array
+  def knuth_shuffle!
+    j = length
+    i = 0
+    while j > 1
+      r = i + rand(j)
+      self[i], self[r] = self[r], self[i]
+      i += 1
+      j -= 1
+    end
+    self
   end
 end
